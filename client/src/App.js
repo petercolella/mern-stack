0a7d058833e49ecec90c0c3204db1bd6f96e1db9
petercolella <peter.joseph.colella@gmail.com>
2019-01-21 23:38:47 -0500

    cleaned things up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
 class App extends Component {
   render() {
@@ -8,21 +6,7 @@ class App extends Component {
       <div className="container">
         <div className="jumbotron">
           <h1 className="display-4">Welcome to the Landing Page!</h1>
-          {/* <header className="App-header"> */}
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p className="lead">
-            {/* Edit <code>src/App.js</code> and save to reload. */}
-            Please Sign In.
-          </p>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-          {/* </header> */}
+          <p className="lead">Please Sign In.</p>
         </div>
       </div>
     );
