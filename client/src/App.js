f6115d4c8286b21a9c609a994f4918a83f1cf4c3
petercolella <peter.joseph.colella@gmail.com>
2019-01-21 22:51:32 -0500

    removed default styling and added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Welcome to the Landing Page!</h1>
+          {/* <header className="App-header"> */}
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p className="lead">
+            {/* Edit <code>src/App.js</code> and save to reload. */}
+            Please Sign In.
           </p>
-          <a
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
+          </a> */}
+          {/* </header> */}
+        </div>
       </div>
     );
   }
