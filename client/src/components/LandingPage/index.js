db35d9c1ff146d3bf2fae171946edb9366bd8a72
petercolella <peter.joseph.colella@gmail.com>
2019-01-23 23:12:56 -0500

    styling on landing page

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+const styles = {
+  marginTop: 120
+};
+
 const LandingPage = () => {
   return (
-    <div className="container">
+    <div style={styles} className="container">
       <div className="jumbotron">
         <h1 className="display-4">Welcome to the Landing Page!</h1>
         <p className="lead">Please Sign In.</p>
