93ac4b320c1d6e29cc482650697f41c7e5cadd79
petercolella <peter.joseph.colella@gmail.com>
2019-01-25 23:37:06 -0500

    added google sign-in code to landing page component

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -11,6 +11,14 @@ const LandingPage = () => {
         <h1 className="display-4">Welcome to the Landing Page!</h1>
         <p className="lead">Please Sign In.</p>
       </div>
+      <div id="gSignInWrapper">
+        <span class="label">Sign in with:</span>
+        <div id="customBtn" class="customGPlusSignIn">
+          <span class="icon" />
+          <span class="buttonText">Google</span>
+        </div>
+      </div>
+      <div id="name" />
     </div>
   );
 };
