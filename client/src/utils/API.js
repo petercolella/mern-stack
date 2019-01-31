6f4d9ed03bb8e8c9bec3806d01928c894b7eec07
petercolella <peter.joseph.colella@gmail.com>
2019-01-31 00:51:40 -0500

    added routes to API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,14 @@ import axios from 'axios';
 export default {
   getUsers: function() {
     return axios.get('/api/users');
+  },
+  getUser: function(id) {
+    return axios.get('/api/users/' + id);
+  },
+  deleteUser: function(id) {
+    return axios.delete('/api/users/' + id);
+  },
+  saveUser: function(userData) {
+    return axios.post('/api/users', userData);
   }
 };
