f6b72b5992da1d7ff585f5720802410915cc2148
petercolella <peter.joseph.colella@gmail.com>
2019-01-31 23:44:09 -0500

    Google sign working and able to grab current user information

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const styles = {
   marginTop: 120
 };
 
-const LandingPage = () => {
-  return (
-    <div style={styles} className="container">
-      <div className="jumbotron">
-        <h1 className="display-4">Welcome to the Landing Page!</h1>
-        <p className="lead">Please Sign In.</p>
+class LandingPage extends Component {
+  state = {
+    currentUser: ''
+  };
+
+  onSignIn = googleUser => {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    if (!profile.getName()) {
+      this.setState({ currentUser: 'Please Sign In.' });
+    } else {
+      this.setState({ currentUser: profile.getName() });
+    }
+  };
+
+  onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+
+    // ******* NEED REDIRECT CODE TO SEND TO PAGE FOR SIGN UP *************
+  }
+  onFailure(error) {
+    console.log(error);
+  }
+
+  componentDidMount() {
+    window.gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+      onsuccess: this.onSignIn,
+      onfailure: this.onFailure
+    });
+  }
+
+  render() {
+    return (
+      <div style={styles} className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Welcome to the Landing Page!</h1>
+          <p className="lead">{this.state.currentUser}</p>
+        </div>
+        <div id="my-signin2" />
       </div>
-      <div id="my-signin2" />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default LandingPage;
