4cf4cad01ddc411e4e2e6180b97def61d39c648e
petercolella <peter.joseph.colella@gmail.com>
2019-02-01 20:38:48 -0500

    minor user list styling

--- client/src/components/UserList/index.js
+++ client/src/components/UserList/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const UserList = props => {
   return (
-    <ul>
-      {props.users.map(user => (
-        <li key={user._id}>
-          <p>Name: {user.name}</p>
-          <p>Email: {user.email}</p>
-          <p>Phone Number: {user.phone}</p>
-        </li>
-      ))}
-    </ul>
+    <div className="container">
+      <h2 className="text-center">User List</h2>
+      <ul className="list-group">
+        {props.users.map(user => (
+          <li key={user._id} className="list-group-item">
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone Number: {user.phone}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
