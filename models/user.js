941c31ca4d12920a0714f5a4ff27aa626a75d30a
petercolella <peter.joseph.colella@gmail.com>
2019-02-02 09:29:26 -0500

    added unique: true to user schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   imageUrl: { type: String },
   phone: { type: String },
   date: { type: Date, default: Date.now }
