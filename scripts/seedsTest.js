71f9ed0ce80dc214125e8db936bb383cec93d27d
petercolella <peter.joseph.colella@gmail.com>
2019-06-27 23:07:47 -0400

    works but process does not exit

--- scripts/seedsTest.js
+++ scripts/seedsTest.js
@@ -48,28 +48,19 @@ db.Symptom.deleteMany({})
   .then(data => {
     data.ops.forEach(body => {
       const symptomsArr = [];
-      body.symptoms.forEach((symptom, i) => {
+      body.symptoms.forEach(symptom => {
         db.Symptom.findOne({ name: symptom.name }, (err, foundSymptom) => {
           if (err) {
             console.log('Find Symptom Err: ', err);
             process.exit(1);
           }
           symptomsArr.push(foundSymptom._id);
-          //   db.Body.findOneAndUpdate(
-          //     { _id: body._id },
-          //     { $push: { symptoms: foundSymptom._id } },
-          //     { new: true }
-          //   ).then(result =>
-          //     console.log('findOneAndUpdate result.symptoms\n', result.symptoms)
-          //   );
-          //   console.log('symptomsArr', symptomsArr, '\nbody', body);
         }).then(() => {
           db.Body.findOneAndUpdate(
             { _id: body._id },
             { $set: { symptoms: symptomsArr } },
             { multi: true }
           ).then(result => console.log('result', result));
-          console.log('symptomsArr2', symptomsArr, '\nbody', body);
         });
       });
     });
@@ -78,55 +69,3 @@ db.Symptom.deleteMany({})
     console.error(err);
     process.exit(1);
   });
-
-// db.Body.deleteMany({})
-//   .then(() => db.Body.collection.insertMany(bodySeed))
-//   .then(data => {
-//     console.log(data.result.n + ' records inserted!');
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
-
-// db.Symptom.deleteMany({}).then(() => {
-//   db.Symptom.insertMany(symptomSeed, (err, symptoms) => {
-//     if (err) {
-//       console.log('DB Sympton Creation Err: ', err);
-//       process.exit(1);
-//     }
-
-//     db.Body.deleteMany({}).then(() => {
-//       bodySeed.forEach(bodyData => {
-//         let body = new db.Body({
-//           name: bodyData.name,
-//           symptoms: []
-//         });
-//         console.log('new body', body);
-
-//         bodyData.symptoms.forEach(symptomData => {
-//           db.Symptom.findOne(
-//             { name: symptomData.name },
-//             (err, foundSymptom) => {
-//               if (err) {
-//                 console.log('Find Symptom Err: ', err);
-//                 process.exit(1);
-//               }
-
-//               body.symptoms.push(foundSymptom);
-//             }
-//           );
-//         });
-//         body.save((err, savedBody) => {
-//           if (err) {
-//             console.log('Save Body Err: ', err);
-//             process.exit(1);
-//           }
-//           console.log('body post push: ', body);
-//           console.log('savedBody', savedBody);
-//         });
-//       });
-//     });
-//   });
-// });
