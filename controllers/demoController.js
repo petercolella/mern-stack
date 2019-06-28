96b2a4cc6f324506a9652c937b027ac7277c3d11
petercolella <peter.joseph.colella@gmail.com>
2019-06-27 23:19:47 -0400

    adds api get route for bodies

--- controllers/demoController.js
+++ controllers/demoController.js
@@ -1,5 +1,15 @@
+const db = require('../models');
+
 module.exports = {
   hello: function(req, res) {
     res.json('Hello World!');
+  },
+  getBodies: function(req, res) {
+    db.Body.find()
+      .populate('symptoms')
+      .then(dbModel => {
+        res.json(dbModel);
+      })
+      .catch(err => res.status(422).json(err));
   }
 };
--- routes/api/demoRoute.js
+++ routes/api/demoRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const demoController = require('../../controllers/demoController');
 
 router.route('/').get(demoController.hello);
+router.route('/bodies').get(demoController.getBodies);
 
 module.exports = router;
