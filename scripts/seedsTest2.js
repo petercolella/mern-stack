99f2a5e3942cc8fb70f1e705a49693c445a07f1c
petercolella <peter.joseph.colella@gmail.com>
2019-06-29 20:02:02 -0400

    seedsTest2.js putting 1 symptom in symptoms field

---
+++ scripts/seedsTest2.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const db = require('../models');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/demoDB', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+mongoose.set('useCreateIndex', true);
+
+const bodySeed = [
+  {
+    name: 'Head',
+    symptoms: []
+  },
+  {
+    name: 'Torso',
+    symptoms: []
+  }
+];
+
+const symptomSeed = [
+  {
+    name: 'Pain'
+  },
+  {
+    name: 'Swelling'
+  }
+];
+
+db.Symptom.deleteMany({})
+  .then(() => db.Symptom.collection.insertMany(symptomSeed))
+  .then(() => db.Body.deleteMany({}))
+  .then(() => db.Body.collection.insertMany(bodySeed))
+  .then(data => {
+    data.ops.forEach(body => {
+      console.log('body', body);
+      db.Symptom.find({}, (err, foundSymptoms) => {
+        if (err) {
+          console.log('Find Symptom Err: ', err);
+          process.exit(1);
+        }
+        foundSymptoms.forEach(symptom => {
+          console.log('symptom', symptom);
+          db.Body.findOneAndUpdate(
+            { _id: body._id },
+            { $set: { symptoms: symptom._id } },
+            { multi: true },
+            (err, updatedBody) => {
+              if (err) {
+                console.log('Update Body Err: ', err);
+                process.exit(1);
+              }
+            }
+          );
+        });
+      });
+    });
+  });
