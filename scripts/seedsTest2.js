f457ba5d8d23859ea443dcdcd41cb5612d767d2d
petercolella <peter.joseph.colella@gmail.com>
2019-07-09 23:42:19 -0400

    seeds correctly, but w/o process exit

--- scripts/seedsTest2.js
+++ scripts/seedsTest2.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/demoDB', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
 mongoose.set('useCreateIndex', true);
 
 const bodySeed = [
@@ -27,49 +28,22 @@ const symptomSeed = [
   }
 ];
 
-mongoose.Model.findSymptoms = function() {
-  let promise = new Promise((resolve, reject) => {
-    this.find({}, function(err, foundSymptoms) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(foundSymptoms);
-      }
-    });
-  });
-  return promise;
-};
-
-mongoose.Model.updateBody = function(body, symptom) {
-  let promise = new Promise((resolve, reject) => {
-    this.findOneAndUpdate(
-      { _id: body._id },
-      { $push: { symptoms: symptom._id } },
-      { multi: true },
-      function(err, updatedBody) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(updatedBody);
-        }
-      }
-    );
-  });
-  return promise;
-};
-
 db.Symptom.deleteMany({})
   .then(() => db.Symptom.collection.insertMany(symptomSeed))
   .then(() => db.Body.deleteMany({}))
   .then(() => db.Body.collection.insertMany(bodySeed))
   .then(data => {
     data.ops.forEach(body => {
-      console.log('body', body);
-      return db.Symptom.findSymptoms().then(foundSymptoms => {
+      return db.Symptom.find({}, (err, foundSymptoms) => {
+        if (err) process.exit(1);
         foundSymptoms.forEach(symptom => {
-          console.log('symptom', symptom);
-          return db.Body.updateBody(body, symptom)
-            .then(() => process.exit(0))
+          return db.Body.findOneAndUpdate(
+            { _id: body._id },
+            { $push: { symptoms: symptom._id } },
+            { new: true }
+          )
+            .exec()
+            .then(updatedBody => console.log('updatedBody', updatedBody))
             .catch(err => {
               console.log('Error: ', err.message);
               process.exit(1);
