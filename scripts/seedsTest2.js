4a8d3bfdf8a785e27378ef450cac73fc419c6b2c
petercolella <peter.joseph.colella@gmail.com>
2019-07-18 23:08:37 -0400

    reverts seedsTest2 to early version w/o promises

--- scripts/seedsTest2.js
+++ scripts/seedsTest2.js
@@ -33,69 +33,23 @@ db.Symptom.deleteMany({})
   .then(() => db.Body.deleteMany({}))
   .then(() => db.Body.collection.insertMany(bodySeed))
   .then(data => {
-    const promises = data.ops.map(body => {
-      return body;
-      //   return db.Symptom.find().exec();
-      // .then(foundSymptoms => {
-      //   console.log('foundSystems', foundSymptoms);
-      //   return foundSymptoms;
-      // });
-      //   .then(foundSymptoms => {
-      //     return foundSymptoms.map(symptom => {
-      //       return db.Body.findOneAndUpdate(
-      //         { _id: body._id },
-      //         { $push: { symptoms: symptom._id } },
-      //         { new: true }
-      //       )
-      //         .exec()
-      //         .then(updatedBody => {
-      //           // console.log('updatedBody', updatedBody);
-      //           return updatedBody;
-      //         });
-      //       //       .catch(err => {
-      //       //         console.log('Error: ', err.message);
-      //       //         process.exit(1);
-      //       //       });
-      //     });
-      //   })
-      // .catch(err => {
-      //   console.log('Error: ', err.message);
-      //   process.exit(1);
-      // });
-    });
-    Promise.all(promises).then(res => {
-      console.log('res', res);
-      res.map(item => {
-        console.log('item', item);
-        // item.map(item2 => {
-        //   console.log('item2', item2);
-        //   //   item2.then(final => {
-        //   //     console.log('final', final);
-        //   //     return final;
-        //   //   });
-        // });
+    data.ops.map(body => {
+      db.Symptom.find().then(foundSymptoms => {
+        foundSymptoms.map(symptom => {
+          db.Body.findOneAndUpdate(
+            { _id: body._id },
+            { $push: { symptoms: symptom._id } },
+            { new: true }
+          )
+            .then(updatedBody => {
+              console.log('updatedBody', updatedBody);
+              //   process.exit(0);
+            })
+            .catch(err => {
+              console.log('Error: ', err.message);
+              process.exit(1);
+            });
+        });
       });
-      //   process.exit(0);
     });
-    // console.log('promises', promises);
-    // Promise.all(promises).then(promiseArr => {
-    //   //   console.log('resArr', resArr);
-    //   //   Promise.all(resArr).then(res => {
-    //   //     console.log('res', res);
-    //   //   });
-    //   return promiseArr.map(res => {
-    //     // console.log('res', res);
-    //     return res.map(promise => {
-    //       console.log('promise', promise);
-    //       return promise
-    //         .then(secRes => {
-    //           console.log('2nd res', secRes);
-    //           return secRes;
-    //         })
-    //         // .then(() => process.exit(0));
-    //       //   process.exit(0);
-    //     });
-    //     // Promise.all(promisePromises).then(process.exit(0));
-    //   });
-    // });
   });
