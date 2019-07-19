aacd3d365be57e13a22c795ae6932c2dfc3926b2
petercolella <peter.joseph.colella@gmail.com>
2019-07-18 23:09:58 -0400

    cleans up seedsTest3; console logs remain for demo

--- scripts/seedsTest3.js
+++ scripts/seedsTest3.js
@@ -28,48 +28,39 @@ const symptomSeed = [
   }
 ];
 
-let bodyIds = [];
-let symptomIds = [];
+let bodyIds;
+let symptomIds;
 
 function updateBody(bodyIds, symptomIds) {
   const updatePromises = bodyIds.map(bodyId => {
     return symptomIds.map(symptomId => {
-      return (
-        db.Body.findOneAndUpdate(
-          { _id: bodyId },
-          { $push: { symptoms: symptomId } },
-          { new: true }
-        )
-          // .then(updatedBody => {
-          //   updatedBody;
-          // })
-          .catch(err => {
-            console.log('Error: ', err.message);
-          })
-      );
+      return db.Body.findOneAndUpdate(
+        { _id: bodyId },
+        { $push: { symptoms: symptomId } },
+        { new: true }
+      )
+        .then(result => {
+          return result;
+        })
+        .catch(err => {
+          console.log('Error: ', err.message);
+          process.exit(1);
+        });
     });
   });
-  Promise.all(updatePromises).then(a => {
-    console.log('a', a, '\n');
-    const newArr = [];
-    a.forEach(element => {
-      console.log('element', element, '\n');
-      element.forEach((element2, i) => {
-        console.log('** element2', i, element2, '\n');
-        newArr.push(element2);
-        console.log('newArr', newArr, '\n');
-        //         element2.then(element3 => {
-        //           console.log('element3', element3, '\n');
-        //           return element3;
-        //         });
+  Promise.all(updatePromises).then(outerArr => {
+    console.log('outerArr', outerArr, '\n');
+    const newPromiseArr = [];
+    outerArr.forEach(innerArr => {
+      console.log('innerArr', innerArr, '\n');
+      innerArr.forEach(promise => {
+        console.log('promise', promise, '\n');
+        newPromiseArr.push(promise);
+        console.log('newPromiseArr', newPromiseArr, '\n');
       });
-      //         Promise.all(promisePromises).then(b => {
-      //           console.log('b', b, '\n');
-      //         });
-      //       .then(() => process.exit(0));
     });
-    Promise.all(newArr).then(res => {
-      console.log('res', res, '\n');
+    Promise.all(newPromiseArr).then(results => {
+      console.log('results:', results, '\n');
       process.exit(0);
     });
   });
